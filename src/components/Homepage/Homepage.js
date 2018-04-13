7f3e813b26b85e9de4c290639b8774b9add055a4
Haldun Anil <haldunanil@gmail.com>
2018-04-12 23:07:53 -0400

    Create Homepage component and add it to App.js

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Homepage.css";
+
+class Homepage extends Component {
+  render() {
+    return <div>Hello, is it me you're looking for?</div>;
+  }
+}
+
+export default Homepage;
