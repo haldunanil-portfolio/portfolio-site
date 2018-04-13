c2b35a854cde9a759706491ef9c8610c748e5b78
Haldun Anil <haldunanil@gmail.com>
2018-04-12 23:16:41 -0400

    Moved Footer out to App.js since it doesn't vary by page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Homepage from "../Homepage/Homepage";
+import Footer from "../Footer/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Homepage />
+        <Footer />
       </div>
     );
   }
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
 import "./Homepage.css";
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
 
 class Homepage extends Component {
   render() {
     return (
-        <div>
-            <Header/>
-            Hello, is it me you're looking for?
-            <Footer/>
-        </div>
+      <div>
+        <Header />
+        Hello, is it me you're looking for?
+      </div>
     );
   }
 }
