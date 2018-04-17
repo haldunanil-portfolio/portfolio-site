d2b4323260bc8eb4fd0f7d0a254b8bd4ff12333b
Haldun Anil <haldunanil@gmail.com>
2018-04-16 22:05:01 -0400

    Add more things to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,14 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      settings: {
+        primaryBackgroundColor: "#FFFFFF",
+        secondaryBackgroundColor: "#f5f5f5",
+        accentBackgroundColor: "#E22F37",
+        primaryTextColor: "#2D2C2D",
+        secondaryTextColor: "#FFFFFF",
+        highlightTextColor: "#2B9EB3"
+      },
       nav: [
         {
           label: "Home",
@@ -36,11 +44,48 @@ class App extends Component {
         }
       ],
       homePage: {
-        headerBgImg: "/hal.jpg",
-        headerByline: "Who am I and why are you on this website?",
-        headerHeadline: "HELLO, IS IT ME YOU'RE LOOKING FOR?",
-        headerCallToAction: "CALL TO ACTION",
-        headerCallToActionHref: "/"
+        header: {
+          backgroundImage: "/hal.jpg",
+          byline: "Who am I and why are you on this website?",
+          headline: "HELLO, IS IT ME YOU'RE LOOKING FOR?",
+          callToAction: "CALL TO ACTION",
+          callToActionHref: "/"
+        },
+        aboutMeSection: {
+          header: "About me",
+          content: "I strongly believe that being true to myself, my passions, and my interests is my strongest quality as an individual. I am never satisfied with the status quo and am always thinking about new ways to push the envelope, think creatively about the challenges I face, and dare to do things differently. I am not shy about expressing my opinions and am always on the lookout to do whatever I can to improve the world around me."
+        },
+        webDevelopmentSection: {
+          header: "Some picks from my web development portfolio",
+          projects: [
+            {
+              imageSrc: "/minesweeper-screenshot.png",
+              imageAlt: "Minesweeper game screenshot",
+              title: "MinesweepeReact",
+              text: "A fresh take on an old classic: play Minesweeper online! Built with React.js.",
+              buttonText: "View Project",
+              buttonHref: "https://haldunanil-portfolio.github.io/minesweepereact/"
+            },
+            {
+              imageSrc: "/jammming-screenshot.png",
+              imageAlt: "Jammming app screenshot",
+              title: "Jammming",
+              text: "Built on top of the Spotify Developer SDK, Jammming allows a user to discover new music, listen, and create new playlists through an easy-to-use interface. Built with React.js,",
+              buttonText: "View Project",
+              buttonHref: "https://haldunanil-portfolio.github.io/jammming/"
+            },
+            {
+              imageSrc: "/ravenous-screenshot.png",
+              imageAlt: "Ravenous app screenshot",
+              title: "Ravenous",
+              text: "A simplified clone of Yelp, use Ravenous whenever you're feeling... well, ravenous! Built with React.js.",
+              buttonText: "View Project",
+              buttonHref: "https://haldunanil-portfolio.github.io/ravenous/"
+            }
+          ],
+            callToAction: "EXPERIENCE MORE",
+            callToActionHref: "/"
+        }
       }
     };
   }
@@ -49,8 +94,8 @@ class App extends Component {
     return (
       <div className="App">
         <NavbarParent data={this.state.nav} />
-        <Homepage options={this.state.homePage} />
-        <FooterParent data={this.state.nav} />
+        <Homepage settings={this.state.settings} data={this.state.homePage} />
+        <FooterParent settings={this.state.settings} data={this.state.nav} />
       </div>
     );
   }
