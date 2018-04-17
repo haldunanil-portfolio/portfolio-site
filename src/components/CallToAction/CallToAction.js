5f64cda06123679f21503975f34a2b409665c9e3
Haldun Anil <haldunanil@gmail.com>
2018-04-17 01:04:53 -0400

    Make CTA open link in new tab

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -5,7 +5,7 @@ import { Button } from "reactstrap";
 class CallToAction extends Component {
   render() {
     return (
-      <a href={this.props.href}>
+      <a href={this.props.href} target="_blank">
         <Button className="Primary-cta">{this.props.children}</Button>
       </a>
     );
