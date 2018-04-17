4ba1cc37a64551734c26f583c32cb72840e99a1f
Haldun Anil <haldunanil@gmail.com>
2018-04-17 01:05:09 -0400

    Make navbar way nicer

--- src/components/NavbarParent/NavbarParent.js
+++ src/components/NavbarParent/NavbarParent.js
@@ -14,15 +14,51 @@ class NavbarParent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: false
+      isOpen: false,
+      isTop: true,
+      navbarBackground: "transparent",
+      navTextColor: "#ffffff",
+      navbarShadow: "none"
     };
+    this.handleStyleChange = this.handleStyleChange.bind(this);
     this.toggle = this.toggle.bind(this);
     this.getNavLinkList = this.getNavLinkList.bind(this);
+  }
+
+  handleStyleChange(condition) {
+    if (condition) {
+      this.setState({
+        navbarBackground: "transparent",
+        navTextColor: "#ffffff",
+        navbarShadow: "none"
+      });
+    } else {
+      this.setState({
+        navbarBackground: "#ffffff",
+        navTextColor: "#2D2C2D",
+        navbarShadow: "0px 2px 5px grey"
+      });
+    }
   }
 
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
+    });
+
+    // change styles as appropriate
+    this.handleStyleChange(this.state.isOpen);
+  }
+
+  componentDidMount() {
+    document.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== this.state.isTop) {
+        this.setState({ isTop });
+
+        // change styles as appropriate
+        this.handleStyleChange(this.state.isTop);
+      }
     });
   }
 
@@ -30,7 +66,11 @@ class NavbarParent extends Component {
     return this.props.data.map(navLink => {
       return (
         <NavItem>
-          <NavLink href={navLink.href} disabled={navLink.disabled}>
+          <NavLink
+            href={navLink.href}
+            disabled={navLink.disabled}
+            style={{ color: navLink.disabled ? null : this.state.navTextColor }}
+          >
             {navLink.label}
           </NavLink>
         </NavItem>
@@ -40,9 +80,18 @@ class NavbarParent extends Component {
 
   render() {
     return (
-      <Navbar expand="md">
-        <WebsiteTitle />
-        <NavbarToggler onClick={this.toggle} />
+      <Navbar
+        fixed="top"
+        expand="md"
+        style={{
+          background: this.state.navbarBackground,
+          WebkitBoxShadow: this.state.navbarShadow,
+          MozBoxShadow: this.state.navbarShadow,
+          boxShadow: this.state.navbarShadow
+        }}
+      >
+        <WebsiteTitle color={this.state.navTextColor} />
+        <NavbarToggler style={{ borderColor: this.state.navTextColor }} onClick={this.toggle} />
         <Collapse isOpen={this.state.isOpen} navbar>
           <Nav className="ml-auto" navbar>
             {this.getNavLinkList()}
